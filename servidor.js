a4d4b9818cee5de775aab9ba52e7b18fa6866a64
BENJAMIN <benjaminmalama@gmail.com>
2020-05-07 12:41:34 +0200

    Primer commit. Hay respuesta de la página Home.

--- servidor.js
+++ servidor.js
@@ -1,21 +1,37 @@
-'use stric'
+'use strict'
 
-//Importar la dependencia y llama a la funcion express()
+//Importamos las librerias y dependencias
+//empezamos con Express
+//https://expressjs.com/es/api.html 4.x.x
 const express = require('express');
-var app = express();
+var app = express()
 
+//Creamos las rutas de nuestro servidor web.
 //Importar las rutas del servidor web empezamos en /
 //  todo esto está en la documentacion de express, puede variar con la version
 // https://expressjs.com/es/4x/api.html express.Router([options])
-var router = express.Router();
-//creamos la ruta
-// https://expressjs.com/es/4x/api.html#router
-router.use(function (req, res, next) {
-  res.send("Bienvenidos a HOME");
-});
-//Activamos la ruta
-app.use(router);
 
+  //Creamos un nuevo objeto ruta
+  //https://expressjs.com/es/api.html#router
+  var router = express.Router();
+
+  //Importamos las rutas de ./login/login.rutas.js
+  const loginRutas = require('./login/login.rutas.js');
+
+  //Creamos la ruta por defecto que es /
+  //https://expressjs.com/es/api.html#router.METHOD
+  router.get('/', (req, res) => {
+    res.send("Bienvenidos a HOME");
+  });
+
+        //Activamos las rutas
+        app.use(router);
+
+
+//Indicar por que puerto escuchará la aplicación ( en este caso express).
+// http://expressjs.com/es/api.html#app.listen
 app.listen(3000, function () {
+  console.log('-----------------------------------------');
   console.log('Ejecutando Servidor Web en el puerto 3000');
+  console.log('-----------------------------------------');
 });
